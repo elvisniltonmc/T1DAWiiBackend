e61d4fdedc08d697454a6ec115664f4d27ee890e
elvisniltonmc <nilton_leo@hotmail.com>
2024-09-26 14:19:20 -0500

    carga inicial de mi evaluacion

---
+++ src/main/java/pe/edu/cibertec/EvaluacionT1DAW_ManchaBackEnd/EvaluacionT1DawManchaBackEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.EvaluacionT1DAW_ManchaBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionT1DawManchaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionT1DawManchaBackEndApplication.class, args);
+	}
+
+}
